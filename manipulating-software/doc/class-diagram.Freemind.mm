8e5534a52e7d8f794352c10b12cee1021b4393dc
vizofan@gmail.com <vizofan@gmail.com@974524e8-606a-e171-89af-5fe8c23ea8ad>
2012-01-09 00:50:17 +0000

    class diagram

---
+++ manipulating-software/doc/class-diagram.Freemind.mm
@@ -0,0 +1,41 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1324361918871" ID="ID_1556055469" MODIFIED="1324362367154" TEXT="ManipulatingSoftware">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1324361967333" ID="ID_81819139" MODIFIED="1324362073326" POSITION="right" TEXT="ResearchSession">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1324362129106" ID="ID_121708538" MODIFIED="1324362169163" TEXT="Dealing with research config file (sessions/SESSIONID.ini)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1324362116339" ID="ID_1047081709" MODIFIED="1324362175711" TEXT="Functions">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1324361983830" ID="ID_1764936816" MODIFIED="1324362123759" TEXT="static Load()">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1324362034369" ID="ID_86862012" MODIFIED="1324362123766" TEXT="static Create()">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1324361993183" ID="ID_696210874" MODIFIED="1324362123769" TEXT="String GetMetadata(what)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1324362013686" ID="ID_495692219" MODIFIED="1324362123772" TEXT="ArrayString GetDataFiles()">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1324363025293" ID="ID_1429365471" MODIFIED="1324363031438" TEXT="SetMetadata(string)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1324363032068" ID="ID_965598178" MODIFIED="1324363037031" TEXT="AddDataFile(path)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
